2e18cad5ae242f6e78461f596e02b1e66bb365df
Landry ZONGO <zongolandry793@gmail.com>
2026-05-23 16:22:43 +0000

    Add child class Student with inheritance and staticmethod

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 class Person:
-   def __init__(self, name,firstname, age, is_active):
-      self.name = name
-      self.firstname = firstname
-      self.age = age
-      self.is_active = is_active
+    def __init__(self, name, firstname, age, is_active):
+        self.name = name
+        self.firstname = firstname
+        self.age = age
+        self.is_active = is_active
 
-   def display_info(self):
-      print(f"Name: {self.name}")
-      print(f"First Name: {self.firstname}")
-      print(f"Age: {self.age}")
-      print(f"Active: {'Yes' if self.is_active else 'No'}")
-      
-   def __str__(self):
-      return f"{self.firstname} {self.name}, Age: {self.age}, Active: {'Yes' if self.is_active else 'No'}"
+    def display_info(self):
+        print(f"Name: {self.name}")
+        print(f"First Name: {self.firstname}")
+        print(f"Age: {self.age}")
+        print(f"Active: {'Yes' if self.is_active else 'No'}")
+
+    def __str__(self):
+        return f"{self.firstname} {self.name}, Age: {self.age}, Active: {'Yes' if self.is_active else 'No'}"
     
+class Student(Person):
+    def __init__(self, name, firstname, age, is_active, field, level, average, student_id):
+        super().__init__(name, firstname, age, is_active)
+        self.student_id = student_id
+        self.field = field
+        self.level = level
+        self.average = average
 
+    def is_admitted(self):
+        return self.average >= 10
+
+    @staticmethod
+    def mention(average):
+        if average >= 16:
+            return "Excellent"
+        elif average >= 14:
+            return "Very Good"
+        elif average >= 12:
+            return "Good"
+        elif average >= 10:
+            return "Passable"
+        else:
+            return "Fail"
