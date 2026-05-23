89c57b87ce28c72cbf2668409762353db57c425b
Landry ZONGO <zongolandry793@gmail.com>
2026-05-23 16:53:22 +0000

    Add child class Teacher with inheritance

--- main.py
+++ main.py
@@ -37,3 +37,19 @@ class Student(Person):
             return "Passable"
         else:
             return "Fail"
+class Teacher(Person):
+    def __init__(self, name, firstname, age, is_active, subjects, grade, hourly_rate, hours_per_week):
+        super().__init__(name, firstname, age, is_active)
+        self.subjects = subjects
+        self.grade = grade
+        self.hourly_rate = hourly_rate
+        self.hours_per_week = hours_per_week
+
+    def display_courses(self):
+        super().display_info()
+        print(f"Subjects: {', '.join(self.subjects)}")
+        print(f"Grade: {self.grade}")
+        print(f"Hourly Rate: {self.hourly_rate}")
+        print(f"Hours per Week: {self.hours_per_week}")
+    def calculate_monthly_salary(self):
+        return self.hourly_rate * self.hours_per_week * 4 
